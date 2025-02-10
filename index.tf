554e476ead6c216169970f924873ffd7e3072381
Tommaso Girotto <tommaso.girotto91@gmail.com>
2025-02-10 21:22:51 +0100

    testing

--- index.tf
+++ index.tf
@@ -1,30 +1,26 @@
-terraform {
-  required_providers {
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
-    }
-    supabase = {
-      source  = "supabase/supabase"
-      version = "1.5.1"
-    }
-    # Netlify does not allow to deploy sites via its provider
-    # netlify = {
-    #   source  = "netlify/netlify"
-    #   version = "~> 0.2.2"
-    # }
-  }
+# The below has been tested, and works well
+# terraform {
+#   required_providers {
+#     digitalocean = {
+#       source  = "digitalocean/digitalocean"
+#       version = "~> 2.0"
+#     }
+#     supabase = {
+#       source  = "supabase/supabase"
+#       version = "1.5.1"
+#     }
+#   }
 
-  # backend "s3" {
-  #   bucket = "your-space-name"     # DigitalOcean Space name
-  #   region = "nyc3"                # Space region (e.g., nyc3, sfo2)
-  #   key    = "path/to/statefile.tfstate"  # Path within the Space (e.g., `myproject/terraform.tfstate`)
-  #   access_key = "your-access-key"  # DigitalOcean access key
-  #   secret_key = "your-secret-key"  # DigitalOcean secret key
-  #   endpoint = "nyc3.digitaloceanspaces.com"  # Endpoint for DigitalOcean Spaces (nyc3, sfo2, etc.)
-  #   acl = "private"               # Make the state file private
-  # }
-}
+#   # backend "s3" {
+#   #   bucket = "your-space-name"     # DigitalOcean Space name
+#   #   region = "nyc3"                # Space region (e.g., nyc3, sfo2)
+#   #   key    = "path/to/statefile.tfstate"  # Path within the Space (e.g., `myproject/terraform.tfstate`)
+#   #   access_key = "your-access-key"  # DigitalOcean access key
+#   #   secret_key = "your-secret-key"  # DigitalOcean secret key
+#   #   endpoint = "nyc3.digitaloceanspaces.com"  # Endpoint for DigitalOcean Spaces (nyc3, sfo2, etc.)
+#   #   acl = "private"               # Make the state file private
+#   # }
+# }
 
 # Variables
 
@@ -59,6 +55,12 @@ variable "supabase_database_password" {
   default     = ""
 }
 
+variable "supabase_build_dir" {
+  description = "Supabase build dir"
+  type        = string
+  default     = ""
+}
+
 # Digital Ocean
 variable "digital_ocean_access_token" {
   description = "Digital ocean access token"
@@ -78,85 +80,51 @@ variable "digital_ocean_project_description" {
   default     = ""
 }
 
-# variable "netlify_access_token" {
-#   description = "Netlify access token"
-#   type        = string
-#   default     = ""
-# }
+variable "netlify_access_token" {
+  description = "Netlify access token"
+  type        = string
+  default     = ""
+}
 
-# variable "netlify_site_name" {
-#   type = string
-# }
-
-# variable "netlify_custom_domain" {
-#   type = string
-# }
+variable "frontend_build_dir" {
+  description = ""
+  type        = string
+  default     = ""
+}
 
 # Providers
-
-# provider "netlify" {
-#   token = var.netlify_access_token
+# provider "digitalocean" {
+#   token = var.digital_ocean_access_token
 # }
 
-provider "digitalocean" {
-  token = var.digital_ocean_access_token
-}
-
-provider "supabase" {
-  access_token = var.supabase_access_token
-}
-
-# 1. Create a Supabase project
-# resource "supabase_project" "test" {
-#   organization_id   = var.supabase_organization_id
-#   name              = var.supabase_project_name
-#   database_password = var.supabase_database_password
-#   region            = var.supabase_project_region
-
-#   lifecycle {
-#     ignore_changes = [
-#       database_password,
-#       instance_size,
-#     ]
-#   }
+# provider "supabase" {
+#   access_token = var.supabase_access_token
 # }
 
-# 2. Create a DigitalOcean project
-resource "digitalocean_project" "playground" {
-  name        = var.digital_ocean_project_name
-  description = var.digital_ocean_project_description
-  purpose     = "Web Application"
-  environment = "Development"
+# Modules
+
+# Backend module
+module "kunene" {
+  source = "git::https://github.com/yebosoftware/kunene.git"
+
+  digital_ocean_access_token = var.digital_ocean_access_token
+  digital_ocean_project_name = var.digital_ocean_project_name
+  digital_ocean_project_description = var.digital_ocean_project_description
+
+  supabase_access_token = var.supabase_access_token
+  supabase_database_password = var.supabase_database_password
+  supabase_organization_id = var.supabase_organization_id
+  supabase_project_name = var.supabase_project_name
+  supabase_project_region = var.supabase_project_region
+  supabase_build_dir = var.supabase_build_dir
 }
 
-resource "digitalocean_app" "golang-sample" {
-  spec {
-    name   = "golang-sample"
-    region = "ams"
+# Frontend module
+module "nile" {
+  source = "git::https://github.com/yebosoftware/nile.git"
+  depends_on = [module.kunene]
 
-    service {
-      name               = "go-service"
-      instance_count     = 1
-      instance_size_slug = "apps-s-1vcpu-1gb"
-
-      git {
-        repo_clone_url = "https://github.com/digitalocean/sample-golang.git"
-        branch         = "main"
-      }
-    }
-  }
-
-  project_id = digitalocean_project.playground.id  # Reference the project created earlier
+  netlify_access_token = var.netlify_access_token
+  frontend_build_dir = var.frontend_build_dir
+  digital_ocean_app_url = module.kunene.digital_ocean_app_url
 }
-
-# 1. Create a Netlify site
-# Since netlify does not offer a proper provider to deploy sites,
-# then we need to use the local provider
-# resource "null_resource" "netlify_deploy" {
-#   provisioner "local-exec" {
-#     command = "netlify sites:create --name ciauu --account-slug tgirotto"
-#     environment = {
-#       NETLIFY_AUTH_TOKEN = var.netlify_access_token
-#     }
-#   }
-# }
